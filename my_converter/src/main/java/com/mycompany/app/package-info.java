baa26b68d0d2c0db0014252184355e17223766f8
Jan Kristel <kristeja.ad.hs-albsig.de>
2024-12-12 15:20:09 +0100

    update project: rename, file structure

--- my_converter/src/main/java/com/mycompany/app/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains the main classes for this application.
-*/
-package com.mycompany.app;
---
+++ my_converter/src/main/java/de/fh/albsig/m100662/App.java
@@ -0,0 +1,40 @@
+package de.fh.albsig.m100662;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+import de.fh.albsig.m100662.ConverterUtils.NumberFactory;
+import de.fh.albsig.m100662.ConverterUtils.NewNumber;
+
+public class App {
+  /**
+  * Die Hauptmethode, die als Einstiegspunkt für die Anwendung dient.
+  * Sie gibt "Hello World!" auf der Konsole aus und führt eine einfache
+  * Addition von zwei Zahlen durch.
+  *
+  * @param args Die Befehlszeilenargumente.
+  */
+  public static void main(final String[] args) {
+    System.out.println("Enter a value and a unit.");
+    Scanner scannerValue = new Scanner(System.in);
+    System.out.println("Value: ");
+    BigDecimal value = scannerValue.nextBigDecimal();
+    Scanner scannerUnit = new Scanner(System.in);
+    System.out.println("Unit: ");
+    String unit = scannerUnit.nextLine();
+
+    NewNumber inputNumber = NumberFactory.createNewNumber(value, unit);
+
+    inputNumber.printNumber();
+
+    if (inputNumber instanceof ConverterUtils.Lengths) {
+      ConverterUtils.Lengths lengthInput = (ConverterUtils.Lengths) inputNumber;
+      lengthInput.printLengthUnits();
+      lengthInput.convertLength();
+      lengthInput.printNumber();
+    }
+
+    scannerValue.close();
+    scannerUnit.close();
+  }
+}
